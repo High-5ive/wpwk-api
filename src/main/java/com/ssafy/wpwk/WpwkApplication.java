8f7d2469aba2d28f7f91ec50e37e8af8ce5edee4
hansol kim <doingnow94@gmail.com>
2021-01-18 16:12:52 +0900

    # 프로젝트 세팅 및 컨트롤러 테스트 완료

---
+++ src/main/java/com/ssafy/wpwk/WpwkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.wpwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpwkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpwkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/wpwk/WpwkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.wpwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpwkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
