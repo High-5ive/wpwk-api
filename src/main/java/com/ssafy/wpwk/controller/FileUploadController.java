b6c8ce30c34569e801130d950c038a4111ccf8d6
hansol kim <doingnow94@gmail.com>
2021-01-18 18:31:07 +0900

    # 파일 업로드 테스트 완료 # 100MB 이상 파일 업로드 성공(1GB이상 대용량 파일 업로드 테스트 예정) # 하나의 파일 처리

---
+++ src/main/java/com/ssafy/wpwk/controller/FileUploadController.java
@@ -0,0 +1,33 @@
+package com.ssafy.wpwk.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class FileUploadController {
+
+    @PostMapping("upload/files")
+    public String uploadMulti(@RequestParam("files[0]")MultipartFile file) throws Exception{
+
+        String basePath = FileSystemView.getFileSystemView().getHomeDirectory().toString() + "/multi";
+
+        //for(MultipartFile file : formData) {
+            String originalName = file.getOriginalFilename();
+            String filePath = basePath + "/" + originalName;
+
+            System.out.println(filePath);
+
+            File dest = new File(filePath);
+            file.transferTo(dest);
+        //}
+        return "uploaded";
+    }
+}
