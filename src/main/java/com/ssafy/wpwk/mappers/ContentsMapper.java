0127341e26cc2fd37d1f02d3a19607b55bd2c88a
hansol kim <doingnow94@gmail.com>
2021-01-19 15:48:25 +0900

    # Contents관련 Mapper관련 코드 구현

--- src/main/java/com/ssafy/wpwk/mappers/ContentsMapper.java
+++ src/main/java/com/ssafy/wpwk/mappers/ContentsMapper.java
@@ -2,11 +2,23 @@ package com.ssafy.wpwk.mappers;
 
 import com.ssafy.wpwk.model.Contents;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Mapper
 public interface ContentsMapper {
 
+    Long create(Contents contents) throws Exception;
 
+    Contents findContents(Long id) throws Exception;
+
+    List<Contents> findAllContents() throws Exception;
+
+    void update(@Param("id")Long id,
+                @Param("contents")Contents contents) throws Exception;
+
+    void delete(Long id) throws Exception;
 }
