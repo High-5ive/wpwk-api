6d619917a5cf47f3692e0b63cc5ea08b12ace39c
TaeSeongMin <goji7309@gmail.com>
2021-01-20 16:26:05 +0900

    # ContentsComment  요청 처리 구현

--- src/main/java/com/ssafy/wpwk/controller/CommentController.java
+++ src/main/java/com/ssafy/wpwk/controller/CommentController.java
@@ -2,12 +2,14 @@ package com.ssafy.wpwk.controller;
 
 
 import com.ssafy.wpwk.model.ContentsComment;
+import com.ssafy.wpwk.service.CommentServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -55,6 +57,7 @@ public class CommentController {
             //회원인지 검증 필요함
             // comment.user.nickname ? jwt.claim.get(nickname)
             //commentService.updateComment(comment);
+            comment.setUpdatedAt(LocalDateTime.now());
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -65,11 +68,13 @@ public class CommentController {
 
     @ApiOperation(value = "컨텐츠 댓글 삭제")
     @DeleteMapping("/comment/{contentsId}")
-    public ResponseEntity<Void> deleteComment(@PathVariable Long contentsId, @RequestBody ContentsComment comment) {
+    public ResponseEntity<Void> deleteComment(@PathVariable Long contentsId,@RequestBody Long commentId) {
         try {
             //회원인지 검증 필요함
             // comment.user.nickname ? jwt.claim.get(nickname)
-            //commentService.deleteComment(comment);
+            //3개의 매개변수 들어가야함
+            //
+            //commentService.deleteComment(contentsId,commentId,userid);
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/ssafy/wpwk/service/CommentService.java
+++ src/main/java/com/ssafy/wpwk/service/CommentService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CommentService {
 
+
     List<ContentsComment> allComments(Long contentsId);
 
 
