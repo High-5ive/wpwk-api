dcced8bd363beb41cfc6f87e730b3f128bf1f322
hansol kim <doingnow94@gmail.com>
2021-01-21 16:44:05 +0900

    # JWT Authentication Filter 구현 및 filter 등록 # User Password 변경 요청 처리 구현

--- src/main/java/com/ssafy/wpwk/config/SecurityConfig.java
+++ src/main/java/com/ssafy/wpwk/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.wpwk.config;
 
+import com.ssafy.wpwk.filters.JWTAuthenticationFilter;
 import com.ssafy.wpwk.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,11 +24,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        Filter filter = new JWTAuthenticationFilter(authenticationManager(), jwtUtil());
+
         http.formLogin().disable()
                 .cors().disable()
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
+                .addFilter(filter)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
